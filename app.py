11ec78709b79e8e9247469c9c7812bbf227edcde
lucasanalysis <121427367+lucasanalysis@users.noreply.github.com>
2022-12-27 12:11:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,10 @@ import pandas
 import numpy
 import streamlit_pandas_profiling
 import streamlit_shap
+import tensorflow
+import cv2
 packages_list=['matplotlib','torch','pandas_profiling','skimage','lime','tqdm','xgboost','lightgbm','openTSNE','shap','bokeh','torchvision','deepchecks',
-              'pandas','numpy','base64','PIL','streamlit_pandas_profiling','streamlit_shap']
+              'pandas','numpy','base64','PIL','streamlit_pandas_profiling','streamlit_shap','tensorflow','cv2']
 for i in packages_list:
   if i in sys.modules.keys():
     st.write(i)
