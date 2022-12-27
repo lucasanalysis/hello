8072c8e437ffae0904a43a992ddd6359ee585d0c
lucasanalysis <121427367+lucasanalysis@users.noreply.github.com>
2022-12-27 11:10:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,10 @@ import torch
 import pandas_profiling
 st.write('hello world')
 import sys
-import keras
 import skimage
 import lime
 import cv2
-packages_list=['matplotlib','torch','pandas_profiling','lime','cv2']
+packages_list=['matplotlib','torch','pandas_profiling','skimage','lime','cv2']
 for i in packages_list:
   if i in sys.modules.keys():
     st.write(i)
