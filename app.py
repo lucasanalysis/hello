188ab4d5b5b398bee8966fe2f906339a7d19ad0a
lucasanalysis <121427367+lucasanalysis@users.noreply.github.com>
2022-12-27 10:55:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import matplotlib
 import torch
+import pandasProfiling
 st.write('hello world')
 import sys
-packages_list=['matplotlib','torch']
+packages_list=['matplotlib','torch','pandasProfiling']
 for i in packages_list:
   if i in sys.modules.keys():
     st.write(i)
