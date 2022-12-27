e4fedc79a24fd92f943f746fb471a2b1b8f9b1e4
lucasanalysis <121427367+lucasanalysis@users.noreply.github.com>
2022-12-27 11:47:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import xgboost
 import lightgbm
 import torchvision
 import bokeh
+import shap
 packages_list=['matplotlib','torch','pandas_profiling','skimage','lime','tqdm','xgboost','lightgbm','pytorch_grad_cam','shap','bokeh','torchvision']
 for i in packages_list:
   if i in sys.modules.keys():
