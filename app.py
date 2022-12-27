97f63e2911db43516003ab229da64accbf9cdc0e
lucasanalysis <121427367+lucasanalysis@users.noreply.github.com>
2022-12-27 12:37:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,10 @@ import streamlit_pandas_profiling
 import streamlit_shap
 import tensorflow
 import cv2
+import clip
+import pytorch_grad_cam
 packages_list=['matplotlib','torch','pandas_profiling','skimage','lime','tqdm','xgboost','lightgbm','openTSNE','shap','bokeh','torchvision','deepchecks',
-              'pandas','numpy','base64','PIL','streamlit_pandas_profiling','streamlit_shap','tensorflow','cv2']
+              'pandas','numpy','base64','PIL','streamlit_pandas_profiling','streamlit_shap','tensorflow','cv2','clip','pytorch_grad_cam']
 for i in packages_list:
   if i in sys.modules.keys():
     st.write(i)
