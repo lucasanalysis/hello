18cdb2247b8f46390f78c2f8ce0bc5ddff422764
lucasanalysis <121427367+lucasanalysis@users.noreply.github.com>
2022-12-27 10:54:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ import streamlit as st
 import matplotlib
 import torch
 st.write('hello world')
+import sys
+packages_list=['matplotlib','torch']
+for i in packages_list:
+  if i in sys.modules.keys():
+    st.write(i)
+st.write('end')
